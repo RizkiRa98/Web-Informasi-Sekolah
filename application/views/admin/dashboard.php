ac5025d00e14d95f3046edf6145fbab52d814b50
Muhamad Rizki Ramadhan <81960526+RizkiRa98@users.noreply.github.com>
2021-05-20 14:58:17 +0700

    Add files via upload

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,140 @@
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- JUMLAH GURU-->
+                        <div class="col-xl-3 col-md-6 mb-4" style="margin:auto;">
+                            <div class="card border-left-primary shadow h-100 py-2 ms-5">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                        <h5 class="card-title">JUMLAH GURU</h5>
+                                                <div class="display-4">55</div>
+                                        </div>
+                                        <div class="col-auto">
+                                         <i class="fas fa-chalkboard-teacher fa-4x"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- JUMLAH SISWA -->
+                        <div class="col-xl-3 col-md-6 mb-4" style="margin:auto;">
+                            <div class="card border-left-success shadow h-100 py-2 ms-5">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                        <h5 class="card-title">JUMLAH SISWA</h5>
+                                                <div class="display-4">980</div>
+                                        </div>
+                                        <div class="col-auto">
+                                        <i class="fas fa-user-graduate fa-4x"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- JUMLAH PEGAWAI -->
+                        <div class="col-xl-3 col-md-6 mb-4" style="margin:auto;">
+                            <div class="card border-left-success shadow h-100 py-2 ms-5">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                        <h5 class="card-title">JUMLAH PEGAWAI</h5>
+                                                <div class="display-4">24</div>
+                                        </div>
+                                        <div class="col-auto">
+                                        <i class="fas fa-user-edit fa-4x"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <!-- Content Row -->
+
+                    <div class="row">
+
+                        <!-- Area Chart -->
+                        <div class="col-xl-12 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between" >
+                                    <h6 class="m-0 font-weight-bold text-primary text-align-center">BERITA MENGENAI SEKOLAH</h6>
+                                    
+                                </div>
+                                <!-- Card Body -->
+                                
+                            </div>
+                        </div>
+
+                        
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Monday Group 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
